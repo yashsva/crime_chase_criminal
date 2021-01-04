36b79cf15a2c4f208904c399e3c93e0f3f31866d
yogeshwarign27 <yogeshwarign27@gmail.com>
2021-01-04 13:56:21 +0530

    added css files and some changes in html file

---
+++ public/javascripts/higher-off.js
@@ -0,0 +1,19 @@
+const { text } = require("body-parser");
+
+function validate()
+{
+    var Username=document.getElementById('userId').value;
+    var Password=document.getElementById('pass').value;
+    var form=document.getElementById('form').value;
+    var error_ele=document.getElementById('error');
+    error_ele.style.padding= 10px;
+    var text;
+
+        if(Username.length <5)
+        {
+            
+        }
+    
+    
+
+}
