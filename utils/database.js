ce277adb6109bf11f4409ce67c66af64194439a6
Yashsva <namansisodiya2013@gmail.com>
2021-07-20 10:51:31 +0530

    changes for azure mysql db hosting

--- utils/database.js
+++ utils/database.js
@@ -3,11 +3,17 @@ const mysql = require('mysql2');
 //To get API keys from .env files (Using process.env.<variable_name> )
 require('dotenv').config();
 
+const ssl_cert=fs.readFileSync(path.join(__dirname,"..","BaltimoreCyberTrustRoot.crt.pem"));
+
+
 const pool=mysql.createPool({
     host:process.env.MYSQL_HOST,
     database:process.env.MYSQL_DATABASE,
     user:process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD   
+    password: process.env.MYSQL_PASSWORD,
+    ssl:{
+        ca:ssl_cert,
+    }  
 });
 
 module.exports=pool.promise(); 
