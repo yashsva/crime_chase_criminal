1242f2c6c468a90e315d88506f4714549055ae88
Yashsva <namansisodiya2013@gmail.com>
2021-09-03 14:00:33 +0530

    new ssl-cert for new cloud server

--- utils/database.js
+++ utils/database.js
@@ -6,7 +6,7 @@ const path = require('path');
 //To get API keys from .env files (Using process.env.<variable_name> )
 require('dotenv').config();
 
-const ssl_cert=fs.readFileSync(path.join(__dirname,"..","BaltimoreCyberTrustRoot.crt.pem"));
+const ssl_cert=fs.readFileSync(path.join(__dirname,"..","DigiCertGlobalRootCA.crt.pem"));
 
 
 const pool=mysql.createPool({
